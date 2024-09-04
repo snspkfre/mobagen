b3f81527b3bcced832d91b595891ec9d18137bcc
Victor <victor.diab@mymail.champain.edu>
2024-09-04 17:04:35 -0400

    Tried to balance the two rules, but not seeing good results.

--- examples/flocking/behaviours/CohesionRule.cpp
+++ examples/flocking/behaviours/CohesionRule.cpp
@@ -3,21 +3,18 @@
 
 Vector2f CohesionRule::computeForce(const std::vector<Boid*>& neighborhood, Boid* boid) {
   Vector2f cohesionForce = Vector2f::zero();
-  
-  // todo: add your code here to make a force towards the center of mass
-  // hint: iterate over the neighborhood
-
-  // find center of mass
-
   Vector2f pos = boid->getPosition();
 
   for (Boid* n : neighborhood)
   {
     if (n == boid) continue;
-    cohesionForce += n->getPosition() - pos;
+    cohesionForce += n->getPosition();
   }
   if (neighborhood.size() > 0)
-  cohesionForce /= neighborhood.size();
-  
+  {
+    cohesionForce /= neighborhood.size();
+    cohesionForce -= pos;
+  }
+
   return cohesionForce;
 }
--- examples/flocking/behaviours/SeparationRule.cpp
+++ examples/flocking/behaviours/SeparationRule.cpp
@@ -6,23 +6,18 @@
 Vector2f SeparationRule::computeForce(const std::vector<Boid*>& neighborhood, Boid* boid) {
   // Try to avoid boids too close
   Vector2f separatingForce = Vector2f::zero();
-
-  //    float desiredDistance = desiredMinimalDistance;
-  //
-  //    // todo: implement a force that if neighbor(s) enter the radius, moves the boid away from it/them
-  //    if (!neighborhood.empty()) {
-  //        Vector2f position = boid->transform.position;
-  //        int countCloseFlockmates = 0;
-  //        // todo: find and apply force only on the closest mates
-  //    }
   Vector2f pos = boid->getPosition();
-  Vector2f one(1, 1);
-  for (int i = 0; i < neighborhood.size(); i++)
+
+  for (Boid* n : neighborhood)
   {
-    separatingForce += ((pos - neighborhood[i]->getPosition()).getMagnitude() > desiredMinimalDistance || pos == neighborhood[i]->getPosition()) ? 
-           Vector2f::zero() : (pos - neighborhood[i]->getPosition()).normalized() * (1 / (pos - neighborhood[i]->getPosition()).getMagnitude()); 
+    Vector2f nPos = n->getPosition();
+    Vector2f diff = pos - nPos;
+    float dist = diff.getMagnitude();
+    if (dist > 0 && dist < desiredMinimalDistance)
+    {
+      separatingForce += diff.normalized() * (1 / (dist));
+    }
   }
-  separatingForce = Vector2f::normalized(separatingForce);
 
   return separatingForce;
 }
