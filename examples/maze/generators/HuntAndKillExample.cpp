f709b6d9188d8adc664d2dd3e5483fd8bbd101bc
Victor <victor.diab@mymail.champain.edu>
2024-09-28 00:07:19 -0400

    Added functions to find neighbors.

--- examples/maze/generators/HuntAndKillExample.cpp
+++ examples/maze/generators/HuntAndKillExample.cpp
@@ -4,6 +4,18 @@
 #include <climits>
 bool HuntAndKillExample::Step(World* w) {
   // todo: code this
+  Point2D current;
+  for (int y = 0; y < w->GetSize(); y++)
+  {
+    for (int x = 0; x < w->GetSize(); x++)
+    {
+      current = Point2D(x, y);
+      std::vector<Point2D> neighbors = getVisitables(w, current);
+
+
+    }
+  }
+
   return false;
 }
 void HuntAndKillExample::Clear(World* world) {
@@ -30,7 +42,10 @@ Point2D HuntAndKillExample::randomStartPoint(World* world) {
 std::vector<Point2D> HuntAndKillExample::getVisitables(World* w, const Point2D& p) {
   auto sideOver2 = w->GetSize() / 2;
   std::vector<Point2D> visitables;
-
+  if (p.y > 0 && w->GetNorth(p)) visitables.push_back(Point2D(p.x, p.y - 1));
+  if (p.x < w->GetSize() - 1 && w->GetSouth(p)) visitables.push_back(Point2D(p.x, p.y + 1));
+  if (p.x > 0 && w->GetWest(p)) visitables.push_back(Point2D(p.x - 1, p.y));
+  if (p.y < w->GetSize() - 1 && w->GetEast(p)) visitables.push_back(Point2D(p.x + 1, p.y));
   // todo: code this
 
   return visitables;
@@ -40,6 +55,10 @@ std::vector<Point2D> HuntAndKillExample::getVisitedNeighbors(World* w, const Poi
   auto sideOver2 = w->GetSize() / 2;
   std::vector<Point2D> neighbors;
 
+  if (p.y > 0 && !w->GetNorth(p)) neighbors.push_back(Point2D(p.x, p.y - 1));
+  if (p.x < !w->GetSize() - 1 && w->GetSouth(p)) neighbors.push_back(Point2D(p.x, p.y + 1));
+  if (p.x > 0 && !w->GetWest(p)) neighbors.push_back(Point2D(p.x - 1, p.y));
+  if (p.y < !w->GetSize() - 1 && w->GetEast(p)) neighbors.push_back(Point2D(p.x + 1, p.y));
   // todo: code this
 
   return neighbors;
--- examples/maze/generators/RecursiveBacktrackerExample.cpp
+++ examples/maze/generators/RecursiveBacktrackerExample.cpp
@@ -2,9 +2,33 @@
 #include "Random.h"
 #include "RecursiveBacktrackerExample.h"
 #include <climits>
+#include <vector>
+
+using namespace std;
+
 bool RecursiveBacktrackerExample::Step(World* w) {
   // todo: implement this
-  return false;
+  /*Point2D current;
+
+  for (int y = 0; y < w->GetSize(); y++)
+  {
+    for (int x = 0; x <  w->GetSize(); x++)
+    {
+      current = Point2D(x, y);
+      Point2D other;
+      vector<Point2D> visitables = getVisitables(w, current);
+      if (visitables.size() == 1)
+      {
+        other = visitables.front();
+      }
+      else if (!visitables.empty())
+      {
+        //other =
+      }
+    }
+  }
+
+  return false;*/
 }
 
 void RecursiveBacktrackerExample::Clear(World* world) {
@@ -32,8 +56,10 @@ Point2D RecursiveBacktrackerExample::randomStartPoint(World* world) {
 std::vector<Point2D> RecursiveBacktrackerExample::getVisitables(World* w, const Point2D& p) {
   auto sideOver2 = w->GetSize() / 2;
   std::vector<Point2D> visitables;
-
-  // todo: implement this
+  if (p.y > 0 && w->GetNorth(p)) visitables.push_back(p.Up());
+  if (p.x < w->GetSize() - 1 && w->GetSouth(p)) visitables.push_back(p.Down());
+  if (p.x > 0 && w->GetWest(p)) visitables.push_back(p.Left());
+  if (p.y < w->GetSize() - 1 && w->GetEast(p)) visitables.push_back(p.Right());
 
   return visitables;
 }
