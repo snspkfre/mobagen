bf59b17179e453958fee2dc623d7653a78fedbbe
Victor <victor.diab@mymail.champain.edu>
2024-10-12 16:53:21 -0400

    DFS working for cat.

--- examples/catchthecat/Agent.cpp
+++ examples/catchthecat/Agent.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 std::vector<Point2D> Agent::generatePath(World* w) {
   unordered_map<Point2D, Point2D> cameFrom;  // to build the flowfield and build the path
-  queue<Point2D> frontier;                   // to store next ones to visit
+  priority_queue<Point2D> frontier;                   // to store next ones to visit
   unordered_set<Point2D> frontierSet;        // OPTIMIZATION to check faster if a point is in the queue
   unordered_map<Point2D, bool> visited;      // use .at() to get data, if the element dont exist [] will give you wrong results
 
@@ -29,10 +29,11 @@ std::vector<Point2D> Agent::generatePath(World* w) {
         returnValue.push_back(cur);
         cur = cameFrom[cur];
       }
-/*
+      returnValue.push_back(cur);
+
       for(Point2D n : returnValue) {
         cout << n.x << "," << n.y << endl;
-      }*/
+      }
       return returnValue;
     }
 
